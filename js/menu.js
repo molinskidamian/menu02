e1a708c3de40ada1f46c3b6205a902b6e3a4f3be
Damian Molinski <tedeer@gmail.com>
2021-09-01 16:32:45 +0200

    Problem with class .sidebar in css

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+let searchBtn = document.querySelector(".bx-search");
+
+btn.addEventListener("click", function () {
+  sidebar.classList.toggle('active');
+});
+
+searchBtn.addEventListener("click", function () {
+  sidebar.classList.toggle('active');
+});
