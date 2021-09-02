f7ac61f822bbf11838797f8ff50407ad5005b204
Damian Molinski <tedeer@gmail.com>
2021-09-02 15:27:18 +0200

    sidebar is close and open - work with is yet

--- js/menu.js
+++ js/menu.js
@@ -3,9 +3,9 @@ let sidebar = document.querySelector(".sidebar");
 let searchBtn = document.querySelector(".bx-search");
 
 btn.addEventListener("click", function () {
-  sidebar.classList.toggle('active');
+    sidebar.classList.toggle("active");
 });
 
 searchBtn.addEventListener("click", function () {
-  sidebar.classList.toggle('active');
+    sidebar.classList.toggle("active");
 });
