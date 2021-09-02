ea88c0c91846d3ac4a231c3268b561478d1b5cd2
Damian Molinski <tedeer@gmail.com>
2021-09-02 16:16:29 +0200

    Propably finish project ;p

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,22 @@
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
 let searchBtn = document.querySelector(".bx-search");
+let homeContent = document.querySelector(".home_content");
 
 btn.addEventListener("click", function () {
     sidebar.classList.toggle("active");
+    if (sidebar.classList.contains("active")) {
+        homeContent.classList.add("home_content_left");
+    } else {
+        homeContent.classList.remove("home_content_left");
+    }
 });
 
 searchBtn.addEventListener("click", function () {
     sidebar.classList.toggle("active");
+    if (sidebar.classList.contains("active")) {
+        homeContent.classList.add("home_content_left");
+    } else {
+        homeContent.classList.remove("home_content_left");
+    }
 });
